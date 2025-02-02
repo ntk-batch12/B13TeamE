3d28bbb0e3c6ad92abdf0c7a582d2dbffc7ff496
ntk-batch12 <ahmad.noor24@outlook.com>
2025-02-02 18:53:05 -0500

    Initial Commit

---
+++ src/a/ADemo.java
@@ -0,0 +1,9 @@
+package a;
+
+public class ADemo {
+	
+	public static void main(String[] args) {
+		System.out.println("ADemo: TeamE");
+	}
+
+}
---
+++ src/b/BDemo.java
@@ -0,0 +1,9 @@
+package b;
+
+public class BDemo {
+	
+	public static void main(String[] args) {
+		System.out.println("BDemo: TeamB");
+	}
+
+}
