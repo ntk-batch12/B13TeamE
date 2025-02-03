b767c71fe82e95072b1379cbb0178f755cc10486
zaidalramadhani <zaidalramadhani@localhost>
2025-02-02 21:36:06 -0500

    Added Add method

--- src/b/BDemo.java
+++ src/b/BDemo.java
@@ -5,5 +5,9 @@ public class BDemo {
 	public static void main(String[] args) {
 		System.out.println("BDemo: TeamB");
 	}
+	
+	public void add(int a, int b) { 
+		System.out.println(a + b);
+	}
 
 }
