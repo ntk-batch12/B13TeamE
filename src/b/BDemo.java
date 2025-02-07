03264df561e6263e9e69fb1c38bf495c5ebc23f7
nasrinna <nasrin.ahmad@noorteck.com>
2025-02-06 19:22:08 -0500

    added method

--- src/b/BDemo.java
+++ src/b/BDemo.java
@@ -4,6 +4,11 @@ public class BDemo {
 	
 	public static void main(String[] args) {
 		System.out.println("BDemo: TeamB");
+		
+		
+		int a  = 25 ;
+		int b =20;
+		System.out.println(a + b);
 	}
 
 }
