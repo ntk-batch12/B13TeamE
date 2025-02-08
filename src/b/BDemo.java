e98066edef405ff5211648545e2dd7bb74b44811
emipo <emipo@10.0.0.133>
2025-02-07 17:48:10 -0700

    practice

--- src/b/BDemo.java
+++ src/b/BDemo.java
@@ -5,7 +5,10 @@ public class BDemo {
 	public static void main(String[] args) {
 		System.out.println("BDemo: TeamB");
 	}
-
+public static void divide ( int a, int b) {
+	System.out.println(a/b);
+	
+}
 
 	public static void add ( int a, int b ) {
 	System.out.println ( a + b);
